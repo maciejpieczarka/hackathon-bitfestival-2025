6cbef163cb72e07423c57642b9df8d157acb6186
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2025-11-30 03:00:18 +0100

    feat: connect backend to main screen

--- frontend/components/userCard.tsx
+++ frontend/components/userCard.tsx
@@ -7,20 +7,32 @@ import { ArrowRightIcon } from './ui/icon';
 import { Image } from './ui/image';
 import { Text } from './ui/text';
 
-const UserCard = ({ id, name, activities, image, description }: User) => {
+const UserCard = ({ id, username, activities, description }: User) => {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  let image = '';
+  if (randomNumber === 0) {
+    image = require('@/assets/images/1.jpg');
+  } else if (randomNumber === 2) {
+    image = require('@/assets/images/2.jpg');
+  } else {
+    image = require('@/assets/images/3.jpg');
+  }
+
+  console.log('name: ' + username);
+  console.log('id: ' + id);
   return (
     <Box className="my-4 w-full bg-white shadow-sm flex items-start p-2 rounded-3xl">
       <Image
         className="object-top rounded-3xl object-fill w-full top-0"
         size="2xl"
         source={image}
-        alt={name}
+        alt={username}
       />
       <Box className="px-4">
         <Heading size="md" className="text-left my-2">
-          {name}
+          {username}
         </Heading>
-        <Text>{description.substring(0, 90) + '...'}</Text>
+        <Text>{description ? description.substring(0, 90) : '...'}</Text>
         <Box className="mt-4 flex flex-col justify-start">
           <Heading size="sm">Aktywności</Heading>
           <Box className="flex flex-row gap-2 py-1 flex-wrap">
--- frontend/constants/users.ts
+++ frontend/constants/users.ts
@@ -1,47 +1,8 @@
-import { Activities, Activity } from './activities';
+import { Activity } from './activities';
 
 export type User = {
   id: number;
-  name: string;
-  image: string;
+  username: string;
   description: string;
   activities: Activity[];
 };
-
-export const users: User[] = [
-  {
-    id: 1,
-    name: 'Uzytkownik 1',
-    description:
-      'Lorem ipsum dolor sit amet consecteur Lorem ipsum dolor sit amet consecteur',
-    image: require('@/assets/images/woman.jpg'),
-    activities: [
-      { id: 4, name: 'Rysowanie' },
-      { id: 5, name: 'Malowanie' }
-    ]
-  },
-  {
-    id: 2,
-    name: 'Uzytkownik 2',
-    description:
-      'Lorem ipsum dolor sit amet consecteur Lorem ipsum dolor sit amet consecteur',
-    image: require('@/assets/images/man.jpg'),
-    activities: [Activities[3], Activities[5]]
-  },
-  {
-    id: 3,
-    name: 'Uzytkownik 3',
-    description:
-      'Lorem ipsum dolor sit amet consecteurconsecteurconsecteurconsecteurconsecteurconsecteurconsecteurconsecteurconsecteurconsecteurconsecteurconsecteurconsecteurconsecteur Lorem ipsum dolor sit amet consecteur',
-    image: require('@/assets/images/woman.jpg'),
-    activities: [Activities[7], Activities[8], Activities[5]]
-  },
-  {
-    id: 4,
-    name: 'Uzytkownik 4',
-    description:
-      'Lorem ipsum dolor sit amet consecteur Lorem ipsum dolor sit amet consecteur',
-    image: require('@/assets/images/man.jpg'),
-    activities: [Activities[3], Activities[5]]
-  }
-];

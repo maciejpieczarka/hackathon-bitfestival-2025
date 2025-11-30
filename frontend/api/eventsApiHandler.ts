739be830b85a98a40c6555cc04e8e7168771168d
Kamil-Kowalczyk <kamil2005aaa@gmail.com>
2025-11-30 09:05:12 +0100

    jazdaaaa

--- frontend/api/eventsApiHandler.ts
+++ frontend/api/eventsApiHandler.ts
@@ -1,6 +1,8 @@
 import {URL} from '@/contexts/AuthContext'
+import axios from "axios";
 
 interface IEvents {
+    id: number
     name: string,
     createdAt: string
     category: string,
@@ -8,12 +10,15 @@ interface IEvents {
 }
 
 export async function getEvents(token: string) {
+    // const response = axios.get(URL+'users',
+    //     )
+
     const response = await fetch(URL+'get_events', {
         method: 'GET',
         headers: {
             "Authorization": token,
             "Content-Type": "application/json"
         },
-    })
-    console.log(response)
+    }).then((value)=>value.json()).then(value => console.log(value))
+
 }
